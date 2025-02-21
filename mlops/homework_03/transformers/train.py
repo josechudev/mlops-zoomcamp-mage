aba02f6889699470fff66db52a76a6a2bc4936cd
José Chu <jchudb93@gmail.com>
2025-02-21 21:53:18 +0000

    update train

--- mlops/homework_03/transformers/train.py
+++ mlops/homework_03/transformers/train.py
@@ -4,9 +4,6 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -19,17 +16,20 @@ def transform(
     df: pd.DataFrame, **kwargs
 ): 
     categorical = ['PULocationID', 'DOLocationID']
-    df[categorical] = df[categorical].astype(str)
+    
     train_dicts = df[categorical].to_dict(orient='records')
     dv = DictVectorizer()
     X_train = dv.fit_transform(train_dicts)
 
     target = 'duration'
     y_train = df[target].values
+
     lr = LinearRegression()
     lr.fit(X_train, y_train)
 
+    print(lr.intercept_)
+
     y_pred = lr.predict(X_train)
 
-    return y_pred
+    return dv, lr
 
